8598e3c53dcc1ff9b5e91fb41ba48fac4f390efd
Caio <caio@devari.com.br>
2022-04-30 20:30:38 -0300

    Project setup

---
+++ src/index.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+
+const port = process.env.PORT;
+const mongoString = process.env.MONGO_URI;
+const tokenString = process.env.TOKEN;
+
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+const app = express();
+
+const corsOptions = {
+    origin: ['https://caionunespn.github.io/imi-motivante', 'https://caionunespn.github.io/imi-desmotivante', 'https://caionunespn.github.io/imi-admin']
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+function checkToken(req, res, next) {
+    const auth = req.headers.authorization;
+    if (!auth) {
+        return res.status(400).json({error: 'Invalid token'});
+    } else {
+        const token = auth.split(' ')[1];
+        if (!token) {
+            return res.status(400).json({error: 'Invalid token'});
+        } else {
+            if (token !== tokenString) {
+                return res.status(400).json({error: 'Invalid token'});
+            }
+        }
+        next()
+    }
+}
+
+app.get('/motivantes', checkToken, async function(req, res) {
+    try {
+        mongoose.connection.db.collection('motivante').find({}).toArray(function(err, data){
+            if (err) {
+                throw Error(err);
+            }
+            return res.status(200).json(data);
+        })
+    } catch (err) {
+        return res.status(400).json({error: err.message});
+    }
+});
+
+app.get('/desmotivantes', checkToken, async function(req, res) {
+    try {
+        mongoose.connection.db.collection('desmotivante').find({}).toArray(function(err, data){
+            if (err) {
+                throw Error(err);
+            }
+            return res.status(200).json(data);
+        })
+    } catch (err) {
+        return res.status(400).json({error: err.message});
+    }
+});
+
+app.post('/resposta', checkToken, async function(req, res) {
+    try {
+        const {tipo, ...body} = req.body;
+        if (Object.keys(body).length <= 0) {
+            return res.status(400).json({error: 'Invalid body'});
+        }
+    
+        await mongoose.connection.db.collection(tipo).insertOne(body);
+        return res.status(200).json({ok: true});
+    } catch (err) {
+        return res.status(400).json({error: err.message});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
